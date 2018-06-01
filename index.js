46172116cd10827ba77cdd458b1a18e040afe5b4
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-01 17:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // Imports dependencies and set up http server
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  const express = require('express'),
+  const bodyParser = require('body-parser'),
+  var app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
