426b05270ae3fcd5825e3a26cf5992c7504e4a21
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-02 05:10:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,39 +10,35 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
+
+
+app.post("/webhook", function (req, res) {
+  
   let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      
-      
+  // Make sure this is a page subscription
+  if (body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+     body.entry.forEach(function(entry) {
+       
+      let webhook_event = entry.messaging[0];
+      console.log("GOT: " + webhook_event.message.text);  
+       
+       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        let webhook_event = entry.messaging[0];
-      console.log("GOT: " + webhook_event.message.text);
         if (webhook_event.postback) {
-          processback(event);
+          processPost(event);
         } else if (webhook_event.message) {
           processPostback(event);
         }
       });
     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+    res.sendStatus(200);
   }
-
 });
+
+
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
@@ -70,6 +66,7 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
 
 
 function processPostback(event) {
