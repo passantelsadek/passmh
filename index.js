3e42409c3b3810e07293c71a8f313b91ebe9b9d4
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-02 05:03:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ app.post('/webhook', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log("GOT: " + webhook_event.message.text);
+      
       
       entry.messaging.forEach(function(event) {
+        let webhook_event = entry.messaging[0];
+      console.log("GOT: " + webhook_event.message.text);
         if (webhook_event.postback) {
           processback(event);
         } else if (webhook_event.message) {
