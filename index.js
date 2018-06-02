f3ead60c0fec7b80d08afa2da117022c18d0d2d8
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-02 13:25:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/webhook", function (req, res) {
     // Iterate over each entry
     // There may be multiple entries if batched
      body.entry.forEach(function(entry) {
-      let webhook_event = entry.messaging[0];
+       webhook_event = entry.messaging[0];
       console.log("GOT: " + webhook_event.message.text);  
        
        // Iterate over each messaging event
