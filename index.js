96d497a58cd15873667f31350010edf407f846fd
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-03 23:01:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/webhook", function (req, res) {
         if (event.postback) {
           processPostback(event);
         } else if (event.message) {
-          processReply(event);
+          processPostback(event);
         }
       });
     });
