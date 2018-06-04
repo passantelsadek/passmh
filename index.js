b61438ae0e7e2824d5657704fa3e0ae81f74f733
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-04 03:24:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function processReply(event) {
    console.log(options);
 }
    sendMessage(senderId,{text: JSON.stringify(res.data.items[0].snippet)});
-}
+
       
       
         if (module === require.main) {
