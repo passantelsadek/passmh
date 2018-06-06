dcabdb07d55a56972ac4285ef6d3819e81f413c6
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-06 06:09:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -283,9 +283,6 @@ function sendTextMessage(recipientId, messageText) {
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
 // certificate authority.
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
 module.exports = app;
   
