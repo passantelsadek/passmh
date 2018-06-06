92f64169165cbc8fae1242230b5a4427e52cdc2a
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-06 06:45:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ function processReply(event) {
    console.log(options);
   // sendMessage(senderId,{text: JSON.stringify(res.data.items[0].snippet)});
    sendTextMessage(senderId, JSON.stringify(res.data.items[0].snippet));
+         sendQuickReply(senderId)
    }
       
       
