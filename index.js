5d5488ae2bd4fd9de25285aa718dffbfec8a789a
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-08 05:46:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -389,7 +389,7 @@ function reply(event) {
     
      request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: PAGE_ACCESS_TOKEN},
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
       method: 'POST',
       json: {
         recipient: {id: sender},
