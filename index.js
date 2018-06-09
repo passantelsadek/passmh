4d4917d61f412d65f95c84d5ace84a503b9131bc
passantelsadek <39809073+passantelsadek@users.noreply.github.com>
2018-06-10 00:48:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -405,7 +405,7 @@ app.post('/ai', (req, res) => {
   if (req.body.result.action === 'weather') {
     console.log('*** weather ***');
     let city = req.body.result.parameters['geo-city'];
-    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=c355d6fe8ab3abe2d69f499a6f5147f4&q='+city;
 
     request.get(restUrl, (err, response, body) => {
       if (!err && response.statusCode == 200) {
